6bac97ebdd461e2a9037b04147c5bbba1f5980e0
woosoojin <wsj1998@naver.com>
2019-11-09 05:35:20 +0900

    re

--- trip/settings.py
+++ trip/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'KHUthon',
         'USER': 'postgres',
         'PASSWORD': 'alsrnrdl0849',
-        'HOST': '127.0.0.1',
+        'HOST': 'django-env.b2amge2pjz.us-west-2.elasticbeanstalk.com',
         'PORT': '5432',
     }
 }
